1e6782e64b89604cc33db84437f76079e87c37b7
ky0613 <kyutech0613@gmail.com>
2022-05-01 20:00:42 +0900

    tailwindのcontent変更

--- app/javascript/assets/css/tailwind.config.js
+++ app/javascript/assets/css/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  content: ["./app/views/**/*.erb", "./app/javascript/app.vue"],
+  content: [
+    "./app/views/**/*.erb",
+    "./app/javascript/app.vue",
+    "./app/javascript/packs.vue",
+    "./app/javascript/**/*.vue",
+    "./app/javascript/*.vue",
+  ],
   theme: {
     extend: {},
   },
