82ae06ae4498ab8a217dcb077ee9e25275e7d182
ky0613 <kyutech0613@gmail.com>
2022-05-01 19:06:01 +0900

    プレコンパイル実施

--- app/javascript/assets/css/tailwind.config.js
+++ app/javascript/assets/css/tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
   content: [
     "./app/views/**/*.erb",
     "./app/javascript/app.vue",
-    "./app/javascript/packs.vue",
-    "./app/javascript/**/*.vue",
-    "./app/javascript/*.vue",
+    // "./app/javascript/packs.vue",
+    // "./app/javascript/**/*.vue",
+    // "./app/javascript/*.vue",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
