841c879d101bf9c8c208172e2e084020efd9df9e
ky0613 <kyutech0613@gmail.com>
2022-05-03 06:44:51 +0900

    [ADD] 結果ページのコンポーネント化と再利用性の確保

---
+++ app/javascript/pages/result/ConvertResult.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <TheResult :searchQuery="'激辛'" :kanji="'辛'" :ruby="'から'" :okuri="'い'">
+      <template #message>
+        <div>
+          <p class="text-3xl my-10 mx-auto">
+            特典として，激辛動画をご査収ください。
+          </p>
+        </div>
+      </template>
+    </TheResult>
+  </div>
+</template>
+
+<script>
+import TheResult from "../../components/Result/TheResult.vue";
+
+export default {
+  name: "ConvertResult",
+  components: {
+    TheResult,
+  },
+};
+</script>
