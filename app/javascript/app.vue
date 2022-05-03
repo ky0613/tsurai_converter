28f0596a7677d9b8cb6f44d4b1fea638c743cf37
ky0613 <kyutech0613@gmail.com>
2022-05-04 06:54:55 +0900

    レイアウトの変更

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <body class="flex flex-col min-h-screen">
-      <TheHeader />
+      <TheHeader :isColor="isHeaderColor" />
       <main class="flex flex-1">
         <div class="m-auto w-full">
           <router-view />
         </div>
       </main>
-      <TheFooter />
+      <component :is="isFooter" />
     </body>
   </div>
 </template>
@@ -15,11 +15,39 @@
 <script>
 import TheHeader from "./components/TheHeader";
 import TheFooter from "./components/TheFooter";
+import ResultFooter from "./components/ResultFooter.vue";
 
 export default {
   components: {
     TheHeader,
     TheFooter,
+    ResultFooter,
+  },
+  computed: {
+    isHeaderColor() {
+      switch (this.$route.path) {
+        case "/":
+          return "";
+        case "/fire-result":
+          return "pink";
+        case "/convert-result":
+          return "red";
+        case "/twin-result":
+          return "orange";
+      }
+    },
+    isFooter() {
+      switch (this.$route.path) {
+        case "/":
+          return "TheFooter";
+        case "/fire-result":
+          return "ResultFooter";
+        case "/convert-result":
+          return "ResultFooter";
+        case "/twin-result":
+          return "ResultFooter";
+      }
+    },
   },
 };
 </script>
